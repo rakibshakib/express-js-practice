149ab54c4f218194930e6bdd393dd3f898f31178
rakibshakib <rakibshakib74@gmail.com>
2022-12-17 00:29:46 +0600

    added view page on server

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+app.set("view engine", "ejs"); //setting view engine in backend server..
+// app.set("views", "template") - if custom template need 
+
+// avoid error 
+mongoose.set("strictQuery", true);
 
 // middle ware
 app.use(morgan("dev"));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// route
+app.get("/", async (req, res) => {
+  res.render("home")
+});
+// connecting to database
+mongoose
+  .connect("mongodb://localhost:27017/rakib", { useNewUrlParser: true })
+  .then(() => {
+    // console.log("database connecting");
+    app.listen(8080, () => {
+      console.log("application is ready to serve on port 8080....");
+    });
+  })
+  .catch((err) => {
+    console.log("error message from database", err);
+  });
